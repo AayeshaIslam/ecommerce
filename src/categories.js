9d87cf0e75849b389362307a4858e1d0813e07ac
AayeshaIslam <92817486+AayeshaIslam@users.noreply.github.com>
2024-03-07 19:23:21 -0500

    started navbar

---
+++ src/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+    {name: 'New', path: '/collection/new'},
+    {name: 'Tops', path: '/collection/tops'},
+    {name: 'Spring break Collection', path: '/collection/springbreak'},
+    {name: 'Dresses', path: '/collection/dresses'}
+];
+
+export default categories;
